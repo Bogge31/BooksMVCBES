dcb5167a3f441bb0ace67fb7beee80b93f64d17f
Bogge31 <34136797+Bogge31@users.noreply.github.com>
2020-05-29 22:18:40 +0000

    End of class chanes

--- BooksMVC/Controllers/BooksController.cs
+++ BooksMVC/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using BooksMVC.Domain;
 using BooksMVC.Models.Books;
@@ -16,6 +17,86 @@ namespace BooksMVC.Controllers
         public BooksController(BooksDataContext dataContext)
         {
             _dataContext = dataContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromInventory(int bookId)
+        {
+            var book = await _dataContext.Books.SingleOrDefaultAsync(b => b.Id == bookId);
+            if (book == null)
+            {
+                TempData["flash"] = "Couldn't find book";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                book.InInventory = !book.InInventory;
+                await _dataContext.SaveChangesAsync();
+                TempData["flash"] = $"Removed {book.Title} from inventory.";
+                return RedirectToAction("Index");
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            // TODO: If it isn't there, send a 404
+            var bookToEdit = await _dataContext.Books
+                .Select(b => new BookEdit
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    NumberOfPages = b.NumberOfPages,
+                    PublicationDate = new DateTime(1964, 1, 1)
+                })
+                .SingleOrDefaultAsync(b => b.Id == id);
+            return View(bookToEdit);
+        }
+
+        public async Task<IActionResult> Update(BookEdit editedBook)
+        {
+            // TODO: Validate it, update it, redirect
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", editedBook);
+            }
+            else
+            {
+                var storedBook = await _dataContext.Books.SingleOrDefaultAsync(b => b.Id == editedBook.Id);
+                storedBook.Title = editedBook.Title;
+                storedBook.Author = editedBook.Author;
+                storedBook.NumberOfPages = editedBook.NumberOfPages;
+                await _dataContext.SaveChangesAsync();
+                TempData["flash"] = $"Updated {storedBook.Title}";
+                return RedirectToAction("Index");
+            }
+        }
+
+        public IActionResult New()
+        {
+            return View(new BookCreate());
+        }
+
+        public async Task<IActionResult> Create(BookCreate bookToAdd)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("New", bookToAdd);
+            }
+            else
+            {
+                var book = new Book
+                {
+                    Title = bookToAdd.Title,
+                    Author = bookToAdd.Author,
+                    InInventory = true,
+                    NumberOfPages = bookToAdd.NumberOfPages
+                };
+                _dataContext.Books.Add(book);
+                await _dataContext.SaveChangesAsync();
+                TempData["flash"] = $"Book {book.Title} add as {book.Id}";
+                return RedirectToAction("New"); // PRG
+            }
         }
 
         [HttpGet("/books/{bookId:int}")]
@@ -49,7 +130,7 @@ namespace BooksMVC.Controllers
             // NO Model.  Just serializing the domain objects.
             //var response = await _dataContext.Books.Where(b => b.InInventory).ToListAsync();
             //return View(response);
-
+            ViewData["sale"] = "All Books are 20% Off Unitl Friday";
             var response = new GetBooksResponseModel
             {
                 Books = await _dataContext.Books.Where(b => b.InInventory).Select(b => new BooksReponseItemModel
